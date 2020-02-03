38b20c49a67bd5627c528657bafeff535fef6e93
JohnKostanov <kostanovd@icloud.com>
2020-02-03 21:38:53 +0100

    Add new json file

--- Landmarks/LandmarkRow.swift
+++ Landmarks/LandmarkRow.swift
@@ -11,7 +11,7 @@ import SwiftUI
 
 struct LandmarkRow: View {
     var landmark: Landmark
-
+    
     var body: some View {
         HStack {
             landmark.image
@@ -19,6 +19,11 @@ struct LandmarkRow: View {
                 .frame(width: 50, height: 50)
             Text(landmark.name)
             Spacer()
+            
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .imageScale(.medium)
+            }
         }
     }
 }
