116810b55bf5cd62ae63de257be62a07988e5bff
JohnKostanov <kostanovd@icloud.com>
2020-02-06 23:45:30 +0100

    Add Compose Animations for Complex Effects

--- Landmarks/HikeView.swift
+++ Landmarks/HikeView.swift
@@ -8,10 +8,18 @@
 
 import SwiftUI
 
+extension AnyTransition {
+    static var moveAndFade: AnyTransition {
+        let insertion = AnyTransition.move(edge: .trailing)
+            .combined(with: .opacity)
+        let removal = AnyTransition.scale
+            .combined(with: .opacity)
+        return .asymmetric(insertion: insertion, removal: removal)    }
+}
 
 struct HikeView: View {
     var hike: Hike
-    @State private var showDetail = false
+    @State private var showDetail = true
     
     var body: some View {
         VStack {
@@ -45,6 +53,7 @@ struct HikeView: View {
 
             if showDetail {
                 HikeDetail(hike: hike)
+                    .transition(.moveAndFade)
             }
         }
     }
