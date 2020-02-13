1a27cb5dc312e01492ed55f9ef101c2ab4fe73ac
JohnKostanov <kostanovd@icloud.com>
2020-02-14 00:32:57 +0100

    Create View to Represent a UIPageViewController

---
+++ Landmarks/Home/PageControl.swift
@@ -0,0 +1,48 @@
+//
+//  PageControl.swift
+//  Landmarks
+//
+//  Created by  Джон Костанов on 14/02/2020.
+//  Copyright © 2020 John Kostanov. All rights reserved.
+//
+
+import SwiftUI
+
+import UIKit
+
+struct PageControl: UIViewRepresentable {
+    var numberOfPages: Int
+    @Binding var currentPage: Int
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIView(context: Context) -> UIPageControl {
+        let control = UIPageControl()
+        control.numberOfPages = numberOfPages
+        control.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.updateCurrentPage(sender:)),
+            for: .valueChanged)
+
+        return control
+    }
+
+    func updateUIView(_ uiView: UIPageControl, context: Context) {
+        uiView.currentPage = currentPage
+    }
+
+    class Coordinator: NSObject {
+        var control: PageControl
+
+        init(_ control: PageControl) {
+            self.control = control
+        }
+
+        @objc
+        func updateCurrentPage(sender: UIPageControl) {
+            control.currentPage = sender.currentPage
+        }
+    }
+}
---
+++ Landmarks/Home/PageViewController.swift
@@ -0,0 +1,27 @@
+//
+//  PageViewController.swift
+//  Landmarks
+//
+//  Created by  Джон Костанов on 13/02/2020.
+//  Copyright © 2020 John Kostanov. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageViewController: UIViewControllerRepresentable {
+    var controllers: [UIViewController]
+    
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal)
+
+        return pageViewController
+    }
+    
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers(
+            [controllers[0]], direction: .forward, animated: true)
+    }
+}
